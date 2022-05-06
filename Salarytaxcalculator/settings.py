d2e96ee64bf064f715f5e61b359ee030c1c46c01
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-05-06 13:20:47 +0545

    added all modified files

--- Salarytaxcalculator/settings.py
+++ Salarytaxcalculator/settings.py
@@ -132,7 +132,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = "/images/"
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'index'
 
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .form import UserRegisterForm
+from .form import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
+
 
 def register(request):
     if request.method == "POST":
@@ -17,7 +18,24 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
-
+    if request.method == "POST":
+        form1 = UserUpdateForm(request.POST, instance=request.user)
+        form2 = ProfileUpdateForm(request.POST,
+                                  request.FILES,
+                                  instance=request.user.profile)
+        if form1.is_valid() and form2.is_valid():
+            form1.save()
+            form2.save()
+            messages.success(request, f'Your account has been Updated!')
+            return redirect("profile")
+    else:
+        form1 = UserUpdateForm(instance=request.user)
+        form2 = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        "user_form": form1,
+        "profile_form": form2,
+    }
+    return render(request, 'users/profile.html', context)
